b53cabb0d3b7881dbc8e47ee229ccb9674b50a16
Caesar453 <160361989+Caesar453@users.noreply.github.com>
2025-01-05 05:17:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "PALMPAY"
 
-global.banknumber = "8133729715"
+global.banknumber = "915 351 6126"
 
-global.bankowner = "FADARE"
+global.bankowner = "OSAGIE"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
